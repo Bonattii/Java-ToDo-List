63f2cb77f321ca40bf093af51e72c58559d63c8a
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-11-22 21:53:22 -0800

    Finalize the routes, next step -> build html structure

--- src/main/java/com/todolistfinalproject/todolistfinalproject/controllers/TodoListController.java
+++ src/main/java/com/todolistfinalproject/todolistfinalproject/controllers/TodoListController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,41 +51,50 @@ public class TodoListController {
     return "redirect:/tasks";
   }
 
-  // GET http://localhost:8080/tasks/update
+  // GET http://localhost:8080/tasks/update/{id}
   // Shows the page with the form to update a task
-  @GetMapping("/update")
-  public String displayUpdateTaskForm() {
+  // IMPORTANT: th:href="@{/tasks/update/{id}(id=${todolist.id})}"
+  @GetMapping("/update/{id}")
+  public String displayUpdateTaskForm(@PathVariable("id") int id, Model model) {
+    // Try to find the task information by Id
+    Optional<TodoList> todoListOld = todoListRepository.findById(id);
+
+    // If not able to find the task
+    if (!todoListOld.isPresent()) {
+      throw new IllegalArgumentException("Invalid Task.");
+    }
+
+    // Get the content of the old task
+    TodoList todoList = todoListOld.get();
+    model.addAttribute("todolist", todoList);
+
     return "tasks/update-task";
   }
 
   // POST http://localhost:8080/tasks/update/save
-  // IMPORTANT: th:href="@{/tasks/update/save/{id}(id=${todolist.id})}"
-  @PostMapping("/update/save/{id}")
-  public String updateExistingTask(TodoList todoList, Model model) {
-    int id = todoList.getId();
-    // Create a auxiliar todolist class that will get the id from the db
-    TodoList todolistAux = todoListRepository.findById(id).get();
-
-    todolistAux.setTaskName(todoList.getTaskName());
-    todolistAux.setTaskDescription(todoList.getTaskDescription());
-    todoListRepository.save(todolistAux);
+  // IMPORTANT: th:href="@{/tasks/update/save}"
+  @PostMapping("/update/save")
+  public String updateExistingTask(TodoList todolist) {
+    todoListRepository.save(todolist);
 
     return "redirect:/tasks";
   }
 
-  // POST http://localhost:8080/tasks/delete
-  @DeleteMapping("/delete/{id}")
-  public String deleteExistingTask(@PathVariable int id) {
-    // Try to get the id from the header
-    Optional<TodoList> todolist = todoListRepository.findById(id);
+  // GET http://localhost:8080/tasks/delete/{id}
+  // IMPORTANT : th:href="@{/tasks/delete/{id}(id=${todolist.id})}"
+  // OTHER IMPORTANT: th:data-confirm-delete="|You want to delete this task?|"
+  // onclick="if (!confirm(this.getAttribute('data-confirm-delete'))) return
+  // false"
+  @GetMapping("/delete/{id}")
+  public String showDeleteTask(@PathVariable("id") int id, Model model) {
+    // Try to find the task by Id and create a instance of TodoList
+    // If didn't find jus thow an argument exception
+    TodoList todolist = todoListRepository.findById(id)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid Task"));
 
-    // If it is possible to get the id, them delete the task with this id
-    if (todolist.isPresent()) {
-      todoListRepository.delete(todolist.get());
+    todoListRepository.delete(todolist);
 
-      return "redirect:/tasks";
-    } else {
-      throw new RuntimeException("Task not found for the given id.");
-    }
+    return "redirect:/tasks";
+
   }
 }
