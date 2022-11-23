96758fb7d7757e46ce8dea005aeae427de06e6f9
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-11-23 10:47:46 -0800

    Create homecontroller to show the homepage

---
+++ src/main/java/com/todolistfinalproject/todolistfinalproject/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.todolistfinalproject.todolistfinalproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+  @GetMapping("")
+  public String showHomePage() {
+
+    return "home/index";
+  }
+}
