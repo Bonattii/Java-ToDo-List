da24e7183275c41afd8fc100c73db4b1198d3180
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-11-22 17:37:20 -0800

    Initialize Spring

---
+++ src/main/java/com/todolistfinalproject/todolistfinalproject/TodolistfinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.todolistfinalproject.todolistfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistfinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistfinalprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolistfinalproject/todolistfinalproject/TodolistfinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolistfinalproject.todolistfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistfinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
