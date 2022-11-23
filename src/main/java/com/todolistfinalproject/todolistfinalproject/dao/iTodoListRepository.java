e0af29bc1c39c4240e9af5d608e821a6a32eec8c
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-11-22 17:49:12 -0800

    Add comments to better undertanding

--- src/main/java/com/todolistfinalproject/todolistfinalproject/dao/iTodoListRepository.java
+++ src/main/java/com/todolistfinalproject/todolistfinalproject/dao/iTodoListRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.todolistfinalproject.todolistfinalproject.entities.TodoList;
 
 public interface iTodoListRepository extends JpaRepository<TodoList, Integer> {
+  // Overrides the findAll method from the JpaRepository, to says that the type
+  // of the list is the type of TodoList entity
   @Override
   public List<TodoList> findAll();
 }
