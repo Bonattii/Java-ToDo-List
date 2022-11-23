83484aedbc2ad38de59efee28f1fd1a87d476031
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2022-11-22 20:06:24 -0800

    Implement the delete route

--- src/main/java/com/todolistfinalproject/todolistfinalproject/controllers/TodoListController.java
+++ src/main/java/com/todolistfinalproject/todolistfinalproject/controllers/TodoListController.java
@@ -1,12 +1,14 @@
 package com.todolistfinalproject.todolistfinalproject.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -44,12 +46,21 @@ public class TodoListController {
   // POST http://localhost:8080/tasks/update
   @PostMapping("/update/{id}")
   public String updateExistingTask() {
+
     return "";
   }
 
   // POST http://localhost:8080/tasks/delete
   @DeleteMapping("/delete/{id}")
-  public String deleteExistingTask() {
-    return "";
+  public String deleteExistingTask(@PathVariable int id) {
+    Optional<TodoList> todolist = todoListRepository.findById(id);
+
+    if (todolist.isPresent()) {
+      todoListRepository.delete(todolist.get());
+
+      return "redirect:/tasks";
+    } else {
+      throw new RuntimeException("Task not found for the given id.");
+    }
   }
 }
